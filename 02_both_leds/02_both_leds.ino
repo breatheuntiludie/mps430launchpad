cc0c1276d2107d1ef32304ebc9da9254cc5da2c1
breatheuntiludie <zhuchkovprostoivan@gmail.com>
2020-09-20 01:48:28 +0300

    first try

---
+++ 02_both_leds/02_both_leds.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(P1_0, OUTPUT);
+  pinMode(P1_6, OUTPUT);
+
+  Serial.begin(9600); // скорость передачи данных в бит/с PC
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  digitalWrite(P1_0, HIGH);//off red led
+  digitalWrite(P1_6, HIGH);//on green led
+  delay(1000);// sleep 1000 (mk)s
+
+  digitalWrite(P1_6, LOW);
+  digitalWrite(P1_0, LOW);
+  delay(2000);
+}
